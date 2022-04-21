c1689ee67512b97968648e916404cc537949e21e
179452596 <dany.ossandon@duocuc.cl>
2022-04-21 11:54:13 -0400

    views medico,farmaceutico,administrador listas para trabajar

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from .views import login
+from .views import medico
+from .views import farmaceutico
+from .views import administrador
+
+
+
 
 urlpatterns = [
-    path('',login, name="login")
+    path('',login, name="login"),
+    path('medico/',medico, name="medico"),
+    path('farmaceutico/',farmaceutico, name="farmaceutico"),
+    path('administrador/',administrador, name="administrador")
 ]
--- web/views.py
+++ web/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'web/login.html')
+
+def medico(request):
+    return render(request, 'web/medico.html')
+
+def farmaceutico(request):
+    return render(request, 'web/farmaceutico.html')
+
+def administrador(request):
+    return render(request, 'web/administrador.html')
