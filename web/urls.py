dc86a9d11b820c4b85015eeabc7c3173145c211a
Jose Playa <jlarenas.s@gmail.com>
2022-05-03 17:17:49 -0400

    Update

--- web/urls.py
+++ web/urls.py
@@ -1,15 +1,15 @@
+from . import views as main_views
 from django.urls import path
 from .views import login
 from .views import medico
 from .views import farmaceutico
 from .views import administrador
 
-
-
-
 urlpatterns = [
     path('',login, name="login"),
     path('medico/',medico, name="medico"),
     path('farmaceutico/',farmaceutico, name="farmaceutico"),
-    path('administrador/',administrador, name="administrador")
+    path('administrador/',administrador, name="administrador"),
+    path('getCentros/', main_views.getCentros, name="getCentros"),
+    path('signup/', main_views.signup, name="signup")
 ]
